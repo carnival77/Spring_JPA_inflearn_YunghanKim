e3f8b6c8948a80b11fdf146b6f45d2e8b64f5aaf
carnival77 <stam0325@gmail.com>
2023-05-29 22:00:00 +0900

    스프링 핵심 원리 - 기본편. 섹션 9. 빈 스코프

---
+++ core/src/main/java/hello/core/scope/PrototypeBeanA.java
@@ -0,0 +1,8 @@
+package hello.core.scope;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Scope("prototype")
+@Component
+public class PrototypeBeanA {}
